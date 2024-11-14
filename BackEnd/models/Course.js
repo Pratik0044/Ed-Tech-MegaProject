47048d87791d8233a17407b030207bf0eb4462e6
Pratik Vishwakarma <103158578+Pratik0044@users.noreply.github.com>
2024-11-14 18:18:05 +0530

    Complete Creation of all Models

---
+++ BackEnd/models/Course.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose")
+const courseSchema = new mongoose.Schema({
+    courseName:{
+        type:String,
+        trim:true,
+    },
+    courseDescription:{
+        type:String,
+        trim:true,
+    },
+    instructor:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+    whatYouWillLearn:{
+        type:String,
+
+    },
+    courseContent:[{
+        type:mongoose.Schema.type.ObjectId,
+        ref:"Section"
+    }],
+    ratingAndReview:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"RatingAndReview"
+    }],
+    price:{
+        type:Number,
+    },
+    thumbnail:{
+        type:String,
+    },
+    tag:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Tag"
+    },
+    studentsEnrolled:[{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    }]
+
+}) 
+
+module.exports =  mongoose.model("course",courseSchema);
