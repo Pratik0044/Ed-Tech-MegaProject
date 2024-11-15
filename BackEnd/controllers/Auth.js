94b0af7c4373c5179d32d385275a56e46b2cd425
Pratik Vishwakarma <103158578+Pratik0044@users.noreply.github.com>
2024-11-15 23:45:11 +0530

    I have created SignUp controller in EdTech project But verification needs

--- BackEnd/controllers/Auth.js
+++ BackEnd/controllers/Auth.js
@@ -4,6 +4,7 @@ const OTP = require("../models/OTP")
 const otpGenerator = require("otp-generator")
 const emailValidator = require("email-validator")
 const bcrypt = require("bcrypt")
+const Profile = require("../models/Profile")
 // sendOTP
 exports.sendOTP = async (req,res)=>{
     try{
@@ -71,9 +72,25 @@ exports.sendOTP = async (req,res)=>{
 exports.signUp = async (req,res)=>{
     try{
         //data fetch from request ki body
-        const {firstName,lastName,email,mobNo,pass, confirmPass, accountType} = req.body;
+        const {firstName,
+            lastName,
+            email,
+            mobNo,
+            pass, 
+            confirmPass, 
+            accountType,
+            otp,
+    } = req.body;
 
         // validate the email
+        if(!firstName || !lastName|| !email|| !mobNo || !pass || !confirmPass || !otp){
+            console.log("Please enter all feilds:");
+            
+            return res.status(403).json({
+                success:false,
+                message:"All feilds are required"
+            })
+        }
         const emailTest =  emailValidator.validate(email)
         if(!emailTest){
             console.log("Invalid email id:");
@@ -87,7 +104,7 @@ exports.signUp = async (req,res)=>{
         if(pass !== confirmPass){
             return res.status(400).json({
                 success:false,
-                message:"Please enter same Password in both fields"
+                message:"Password and Confirm Password are not matching, Please Try again!"
             })
         }
 
@@ -102,35 +119,53 @@ exports.signUp = async (req,res)=>{
         }
         
         // else
-
-            // find most recent OTP stored for the user
-
-            // validate OTP 
-            
-            // Hashing the password
-        let hashedPass;
-        try{
-            hashedPass = await bcrypt.hash(pass,10);
-
-        }catch(er){
-            console.log("Error in incription of password:",er);
+        // find most recent OTP stored for the user
+        const recentOTP = await OTP.find({email}.sort({createdAT:-1}).limit(1))
+        console.log("Recent OTP : ", recentOTP);
+        
+        // validate OTP 
+        if(recentOTP.length == 0 ){
             return res.status(400).json({
                 success:false,
-                message:"Error in incription of password:"
+                message:"OTP not Found"
             })
-            
+        } else if(recentOTP !== otp){
+            return res.status(400).json({
+                success:false,
+                message:"Invalid OTP"
+            })
         }
 
-            
+        // Hashing the password
+        
+        const hashedPass = await bcrypt.hash(pass,10);
+        
+        // create entry in DB
+        const profileDetails =  await Profile.create({
+            gender:null,dateOfBirth:null,about:null,contactNumber:mobNo
+        })
+        const userData = await User.create({
+            firstName,
+            lastName,
+            email,
+            mobNo,
+            pass:hashedPass,  
+            accountType,
+            additionalDetails:profileDetails._id,
+            image:`https://api.dicebear.com/9.x/initials/svg?seed=${firstName} ${lastName}`,
+        })
 
+        // return response
 
-            
-
-
+        return res.status(200).json({
+            success:true,
+            message:"User is registered successfully.",
+            userData,
+        })
     }catch(er){
         console.log("An error occure while creating an account:",er);
         
-        res.status(500).json({
+        return res.status(500).json({
             success:false,
             message:"Something went wrong",
         })
@@ -138,5 +173,26 @@ exports.signUp = async (req,res)=>{
 }
 
 // Login
+exports.logIn =async (req,res)=>{
+    try{
+        // fetch data from request ki body
 
+        // validate
+
+        // find user exist or not
+
+        // if not exist return respose user not found try to signUP firstly
+
+        // else
+        // verify the given password with our Db password
+
+        // return res
+    }catch(er){
+        console.log("Something went worn in Login: ",er);
+        return res.status(400).json({
+            success:false,
+            message:"LogIn attenmp fail, Try again later."
+        })
+    }
+}
 // ChangePassword
