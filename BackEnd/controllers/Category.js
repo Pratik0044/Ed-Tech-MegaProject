2c1ba52a06063db3181b6347f77c2c5578e14242
Pratik Vishwakarma <pratikvishwakarma006@gmail.com>
2025-01-28 11:42:54 +0530

    complete every controller

--- BackEnd/controllers/Category.js
+++ BackEnd/controllers/Category.js
@@ -47,3 +47,68 @@ exports.showAllCategorys = async (req,res)=>{
         })
     }
 }
+
+// category page details
+exports.categoryDetails = async (req,res)=>{
+    try{
+
+        // get category id
+        const cotegoryId = req.body;
+
+        // fetch all courses related to given category
+        const selectedCategory = await Category.findById({_id:cotegoryId})
+                                                        .populate("courses")
+                                                        .exec();
+        // validate
+        if(!selectedCategory){
+            return res.status(400).json({
+                success:false,
+                message:"Data not found"
+            })
+        }
+
+        // get the courses with diffrent category
+        const diffrentCategories = await Category.find({_id:{$ne:cotegoryId},}).populate("courses").exec();
+        // get top selling courses
+        const topSellingCourses = await Category.aggregate([
+            { $unwind: "$courses" },
+            {
+                $lookup: {
+                    from: "courses",
+                    localField: "courses",
+                    foreignField: "_id",
+                    as: "courseDetails"
+                }
+            },
+            { $unwind: "$courseDetails" },
+            {
+                $sort: {
+                    "courseDetails.enrolledStudents": -1
+                }
+            },
+            {
+                $group: {
+                    _id: "$_id",
+                    courses: { $push: "$courseDetails" }
+                }
+            },
+            { $limit: 10 }
+        ]);
+        // return res
+
+        return  res.status(200).json({
+            success:true,
+            message:"Category details fetch successfully",
+            data:{
+                selectedCategory:selectedCategory,
+                diffrentCategories:diffrentCategories,
+                topSellingCourses:topSellingCourses
+            }
+        })
+    }catch(er){
+        return res.status(400).json({
+            success:false,
+            message:'Something went wront while fetching category details from DB'
+        })
+    }
+}
