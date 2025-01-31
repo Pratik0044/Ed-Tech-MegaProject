2e496074b9945207687d0e755e59ae207fd909b7
Pratik Vishwakarma <pratikvishwakarma006@gmail.com>
2025-01-31 09:37:05 +0530

    Arranging all data used in App

--- learn_track/src/App.js
+++ learn_track/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <img src="Design.jpeg" className="App-logo" alt="logo" />
+
       <p>
         Edit <code>src/App.js</code> and save to reload.
       </p>
