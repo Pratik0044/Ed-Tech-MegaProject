22fd403621c46e85a70a2eb2c687c3712a17920e
Pratik Vishwakarma <pratikvishwakarma006@gmail.com>
2025-02-01 00:46:07 +0530

    Creation of home page of the project

--- learn_track/src/pages/Home.jsx
+++ learn_track/src/pages/Home.jsx
@@ -2,12 +2,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FaArrowRight } from "react-icons/fa";
+import HighlightText from '../components/core/HomePage/HighlightText';
+import { CTAButton } from '../components/core/HomePage/CTAButton';
+import Banner from '../assets/Images/banner.mp4'
 
 export default function Home() {
   return (
     <div>
         {/* Section 1 */}
-        <div className='relative mx-auto flex flex-col w-11/12 items-center text-white
+        <div className='relative mx-auto max-w-maxContent flex flex-col w-11/12 items-center text-white
          justify-between '>
             <Link to={"/signup"}>
                 <div className='group mt-16 p-1 rounded-full mx-auto bg-richblack-800 font-bold text-richblack-200 
@@ -22,11 +25,32 @@ export default function Home() {
                 </div>
             </Link>
 
-            <div className='font-inter'>
-                Empower Your Future with 
+            <div className='font-inter text-center text-4xl font-semibold mt-8'>
+                Empower Your Future with  
                 <HighlightText text={"Coding and Academic Skills"} />
             </div>
+
+            <div className='w-[90%] text-center mt-4 text-lg font-ligh text-richblack-300'>
             
+            With our online coding courses, you can learn at your own pace, from anywhere in the world, and get access to a 
+            wealth of resources, including hands-on projects, quizzes, and personalized feedback from instructors. 
+            </div>
+
+            <div className='flex flex-row gap-7 mt-8'>
+                <CTAButton active={true} linkto={"/signup"}>Learn More</CTAButton>
+                <CTAButton active={false} linkto={"/signup"}>Book a Demo</CTAButton>
+            </div>
+
+            <div className='shadow-blue-200  mx-3 my-14 '>
+                <video 
+                muted 
+                loop
+                autoPlay
+                >
+                <source src={Banner} type='video/mp4'/>
+                </video>
+            </div>
+
         </div>
         {/* Section 2 */}
 
